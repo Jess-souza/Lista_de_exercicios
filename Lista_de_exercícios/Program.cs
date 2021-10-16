93a297c11592d331f29f2f0ceca542f2ce2a9afb
Jéssica Souza <78058522+Jess-souza@users.noreply.github.com>
2021-10-16 11:05:51 -0300

    Adicionar arquivos de projeto.

---
+++ Lista_de_exercícios/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lista_de_exercícios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
