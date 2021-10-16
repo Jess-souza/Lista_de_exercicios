858909d143dd2a85803a4f87cc701dbf254efea0
Jéssica Souza <78058522+Jess-souza@users.noreply.github.com>
2021-10-16 15:34:59 -0300

    Atualização do Exercicio de IMC

--- Lista_de_exercícios/Program.cs
+++ Lista_de_exercícios/Program.cs
@@ -6,7 +6,54 @@ namespace Lista_de_exercícios
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int opcao;
+            bool validacao = true;
+            
+            do
+            {
+                Console.WriteLine("Qual exercício desejado?");
+                Console.WriteLine("(1) IMC");
+                Console.WriteLine("(2) MediaSalário");
+                Console.WriteLine("(3) Calculo do raio");
+                Console.WriteLine("(4) Alunos aprovados ou reprovados");
+                Console.WriteLine("(5) Animais de estimação");
+                Console.WriteLine("(5) Sair");
+
+                Console.WriteLine("Digite a sua opção: ");
+                opcao = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        float n1, n2, result;
+                        
+                        Console.WriteLine("Digite seu peso (Kg): ");
+                        n1 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite sua altura (m): ");
+                        n2 = float.Parse(Console.ReadLine());
+
+                        result = n1 / (n2 * n2);
+                        if (result < 20)
+                        {
+                            Console.WriteLine("O resultado do IMC: {0}, pessoa abaixo do peso.", Math.Round(result, 2));
+                        }
+                        if (result >= 20 & result < 25)
+                        {
+                            Console.WriteLine("O resultado do IMC: {0}, pessoa com peso ideal.", Math.Round (result, 2));
+                        }
+                        if (result >= 25)
+                        {
+                            Console.WriteLine("O resultado do IMC: {0} pessoa acima do peso.", Math.Round(result, 2));
+                        }
+                        break;
+                }
+                Console.WriteLine("Aperte Enter para continuar");
+                Console.ReadLine();
+                Console.Clear();
+            } while (validacao);
+            {
+            }
         }
+            
     }
 }
